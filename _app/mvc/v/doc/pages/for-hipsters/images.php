db08ddf02bc1c52963e43262c8e35df9264670ba
Laurence Guy SHIC <laurence@shic.fr>
2012-09-10 10:18:54 +0200

    spell checking general documentation update

--- _app/mvc/v/doc/pages/for-hipsters/images.php
+++ _app/mvc/v/doc/pages/for-hipsters/images.php
@@ -2,7 +2,7 @@
 $vv=new VV_doc_page($_vars);
 ?>
 <p>
-    We will describe through step by examples how to do common stuffs with images.<br/><br/>
+    We will describe through step by step examples how to do common stuff with images.<br/><br/>
 
 </p>
 <div class="">
@@ -16,7 +16,25 @@ $vv=new VV_doc_page($_vars);
     </pre>
     <img class="toshop" src="<?=GiveMe::url("pub/app/fmk/img/logo.png")?>"/>
 </div>
+<p>
+    To manipulate images we will use the function <code>GiveMe::imageSized()</code>.<br />
+    This function has <b>five parameters:</b><br/>
+    <ol>
+        <li>url of the original image (required)</li>
+        <li>final width (required)</li>
+        <li>final height (required)</li>
+        <li>background color (optional, black if not specified)</li>
+        <li>MIME type (optional, jpg if not specified)</li>
+    </ol>
 
+<div class="alert alert-info">
+    <h4>Important</h4> 
+    Note that the <code>GiveMe::imageSized()</code> function <b>will crop the image</b> vertically or horizontally 
+    if the width/height ratio of the original image doesn't match the dimensions you want for the new image. 
+    
+</div>
+
+</p>
 <div class="">
     <h4>Basic resize to 200px by 200px </h4>
     <pre class="prettyprint linenums lang-php">
@@ -31,7 +49,7 @@ $vv=new VV_doc_page($_vars);
 </div>
 
 <div class="">
-    <h4>I'need a green background! </h4>
+    <h4>I need a green background! </h4>
     <pre class="prettyprint linenums lang-php">
 <?=htmlentities(
 '<img class="toshop" src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",200,200,"00ff00")?>"/>'
@@ -39,101 +57,116 @@ $vv=new VV_doc_page($_vars);
     </pre>
     <p class="floating">
         <img class="toshop" src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",200,200,"00ff00")?>"/>
-        So the 3rd attribute is a color. Note that there is no # before the hexadeciaml code...right?
+        So the 3rd attribute is a color. Note that there is no # before the hexadecimal code... right?
     </p>
 </div>
 
 <div class="">
-    <h4>No, finaly I prefer the transparent png </h4>
+    <h4>No, finally I prefer the transparent png </h4>
     <pre class="prettyprint linenums lang-php">
 <?=htmlentities(
 '<img class="toshop" 
-    src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",200,200,"transparent","png")?>"/>'
+  src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",200,200,"transparent","png")?>"/>'
 )?>
     </pre>
     <p class="floating">
         <img class="toshop" src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",200,200,"transparent","png")?>"/>
-        So the 3rd attribute is a color...but the keyword <em>transparent</em> works too. The 4th parameter is the mime type.
-        By default the function generates jpegs but if you writte <em>png</em> it will output guess what...a png!
+        So the 3rd attribute is a color... but the keyword <em>transparent</em> works too. The 4th parameter is the mime type.
+        By default the function generates jpegs but if you write <em>png</em> it will output guess what... a png!
     </p>
 </div>
 
-
-
-
-
-<p>
-    It's not easy to manage this urls in a url rewrited project 
-    and it's no more easier to move all urls in your templates when you decide to move your project from a place to another one.
-</p>
-
-<p>
-    Also you will always prefer to display search engine optimized urls than routes.<br/>
-    To manage it, <b>always</b> use this function :
-</p>
-
-<pre class="prettyprint lang-php linenums">
-    GiveMe::url("your-url");
-</pre>
-
-<p>
-    The function GiveMe::url has two parameters:<br>
-    The first one is an url, it can be a relative or absolute url, it can be a file, or a route.<br/>
-    The Second one define if you want to display an absolute or a relative url.<br/>
-</p>
-    
-<div class="alert alert-info">
-    <h4>Important</h4> 
-    We recommend you to <b>never use optimized urls here</b> but routes only.<br>
-    A good reason (not the only one) for it is that optimized urls can be translated, so they could not match according your current language.
-    
+<div class="">
+    <h4>Play with ratios: get a landscape format</h4>
+    <pre class="prettyprint linenums lang-php">
+<?=htmlentities(
+'<img class="toshop" 
+  src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",400,100,"transparent","png")?>"/>'
+)?>
+    </pre>
+    <p class="floating">
+        <img class="toshop" src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",400,100,"transparent","png")?>"/>
+        All you have to do to get a landscape format is to choose a <em>width</em> bigger than the <em>height</em>. <br />
+        In this case, your image will fit horizontally but the top and bottom of the image may be cropped.
+    </p>
 </div>
     
-<p>
-    Here is a list of samples code using <code>GiveMe::url</code> function and the result in html. 
-</p>    
-
-    <?
-    $urls=array(
-        array("url"=>"home/index/toto","comment"=>"Transform route to optimized url"),
-        array("url"=>"i-am/not-a/valid-page","comment"=>"Prevent error when the route is not valid"),
-        array("url"=>"pub/app/fmk/img/logo.png","comment"=>"File exists, so it works"),
-        array("url"=>"app/fmk/img/logo.png","comment"=>"File exists in the pub folder so it works too"),
-        array("url"=>"pub/app/fmk/img/i-am-not-a-real-file.png","comment"=>"File doesn't exists so we manage the error"),
-        array("url"=>"http://www.google.com","comment"=>"<span class='label'>Note</span> Http links will be always displayed..."),
-        array("url"=>"http://i-am-not-a-real-website-url","comment"=>"...even if the target is not a valid website")
-        );
-    ?>
-
-
-<?foreach($urls as $url):?>
-<h5><?=$url["comment"]?></h5> 
-<pre class="prettyprint lang-php linenums">
+<div class="">
+    <h4>Play with ratios, part II: get a portrait format</h4>
+    <pre class="prettyprint linenums lang-php">
 <?=htmlentities(
-'<a href="<?=GiveMe::url("'.$url["url"].'")?>">My Link</a>
-//will generate...
-<a href="'.GiveMe::url($url["url"]).'">My Link</a>
-
-//absolute
-<a href="<?=GiveMe::url("'.$url["url"].'",true)?>">My Link</a>
-//will generate...
-<a href="'.GiveMe::url($url["url"],true).'">My Link</a>'
+'<img class="toshop" 
+  src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",200,300,"transparent","png")?>"/>'
 )?>
-</pre>
+    </pre>
+    <p class="floating">
+        <img class="toshop" src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",200,300,"transparent","png")?>"/>
+        All you have to do to get a landscape format is to choose a <em>height</em> bigger than the <em>width</em>. <br />
+        In this case, your image will fit vertically but the sides of the image may be cropped.
+    </p>
+</div>   
 
-<?endforeach?>
-<?
+<div class="">
+    <h4>Resize images without cropping</h4>
+    <pre class="prettyprint linenums lang-php">
+<?=htmlentities(
+'<img class="toshop" 
+  src="<?=GiveMe::imageSizedWithoutCrop("pub/app/fmk/img/logo.png",200,200,"cdcdcd","jpg")?>"/>'
+)?>
+    </pre>
+    <p class="floating">
+        <img class="toshop" src="<?=GiveMe::imageSizedWithoutCrop("pub/app/fmk/img/logo.png",200,200,"cdcdcd","jpg")?>"/>
+        The function <code>GiveMe::imageSizedWithoutCrop()</code> has the same <b>five</b> parameters than the function<code>GiveMe::imageSized()</code>.<br /><br />
+        The only difference between the two is that <code>GiveMe::imageSizedWithoutCrop()</code> 
+        won't crop your image if the ratio of the dimensions you asked for doesn't match the ratio of the original image:
+        the whole image will be resized to fit in the dimensions you specified.
+    </p>    
+</div>   
 
-?>
+<div class="">
+    <h4>Other examples of resizing without cropping</h4>
+    <p>
+        See how the two functions are resizing the same image with the same <em>width</em> and <em>height</em> specified:
+    </p>
+    <pre class="prettyprint linenums lang-php">
+<?=htmlentities(
+'<img class="toshop" 
+  src="<?=GiveMe::imageSizedWithoutCrop("pub/app/fmk/img/logo.png",100,300,"transparent","png")?>"/>
+<!-- Vs -->
+<img class="toshop" 
+  src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",100,300,"transparent","png")?>"/>'
+)?>
+    </pre>
+    <p class="floating">
+        <img class="toshop" src="<?=GiveMe::imageSizedWithoutCrop("pub/app/fmk/img/logo.png",100,300,"transparent","png")?>"/>
+        <img class="toshop" src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",100,300,"transparent","png")?>"/>
+    </p>
+    
+    <pre class="prettyprint linenums lang-php">
+<?=htmlentities(
+'<img class="toshop" 
+  src="<?=GiveMe::imageSizedWithoutCrop("pub/app/fmk/img/logo.png",300,100,"transparent","png")?>"/>
+<!-- Vs -->
+<img class="toshop" 
+  src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",300,100,"transparent","png")?>"/>'
+)?>
+    </pre>
+    <p class="floating">
+        <img class="toshop" src="<?=GiveMe::imageSizedWithoutCrop("pub/app/fmk/img/logo.png",300,100,"transparent","png")?>"/>
+        <img class="toshop" src="<?=GiveMe::imageSized("pub/app/fmk/img/logo.png",300,100,"transparent","png")?>"/>
+    </p>
+    
+</div>
+
 <div class="alert alert-block alert-success">
-    <h4>Want to know how does it works?</h4>
+    <h4>Want to know how it works?</h4>
 
     <h5>
         Images are in cache!
     </h5>
     <p>
-        Because playing with images is hard for your server, the images are processed once time.<br/>
-        All the processed images files are located in the folder <em> pub/media/cache/img</em>.<br/>
+        Because playing with images is hard for your server, the images are processed only once.<br/>
+        All the processed image files are located in the folder <em> pub/media/cache/img</em>.<br/>
     </p>
 
     <h5>
@@ -141,17 +174,17 @@ $vv=new VV_doc_page($_vars);
     </h5>    
     <p>
 
-        A call to <code>GiveMe::imageSomething(...)</code> will only returns the url of the image, but the serious job will not be done.<br/>
+        A call to <code>GiveMe::imageSomething(...)</code> will only return the url of the image, but the serious job will not be done.<br/>
         The URL you'll get in fact is a route with its own controller, methods etc... 
-        So when a browser go to this url there is two possibilities:<br/><br/>
+        So when a browser go to this url there are two possibilities:<br/><br/>
         
-        <b>First time:</b> The url is not a file, so the .htacces, index.php etc... lead us to the controler, 
-        it parses the url, 
-        process the image, 
-        return it to the browser like a "View"
-        ...and it writtes the file in pub/media/cache/img/route-controler.<br/></br>
+        <b>First time:</b> The url is not a file yet, so the .htacces, index.php etc... lead us to the controller, 
+        that parses the url, 
+        processes the image, 
+        returns it to the browser like a "View"
+        ... and it saves the file in pub/media/cache/img/route-controler.<br/></br>
         
-        <b>Second time :</b> The url is a file cause before it was wrotten, so the file will be returned to the browser normaly without any php processing.<br/>
+        <b>Second time:</b> The url is already a file that was created before, so the file will be returned to the browser normaly without any php processing.<br/>
     </p>
 
 </div>
--- _app/mvc/v/doc/pages/install/upload.php
+++ _app/mvc/v/doc/pages/install/upload.php
@@ -2,6 +2,6 @@
     <li>Upload the files "where-you-want"</li>
     <li>Go to http://where-you-want/setup.php and follow the instructions.</li>
     <li>Go to http://where-you-want</li>
-    <li>It should works...</li>
+    <li>It should work...</li>
     
 </ol>
