6dfe53e862849b08c0d9943ebc7eb4fef597f2a5
rai <francois@shic.fr>
2012-10-24 12:20:58 +0200

    petite modif

--- _app/mvc/v/press/postPreview.php
+++ _app/mvc/v/press/postPreview.php
@@ -14,7 +14,7 @@ $post = $_vars;
                     </div>
                     <br/>
                     <div class="item-description">
-                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim
+                        <?= $post->description?>
                     </div>
                     <br/>
                     <div class="item-link-readMore">
