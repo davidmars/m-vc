ce1c854a9721df3d094092ce1c5333ddbe8ca313
Jerome <jerome@shic.fr>
2012-10-25 14:57:02 +0200

    media / post

--- _app/mvc/v/press/mediaPreview.php
+++ _app/mvc/v/press/mediaPreview.php
@@ -8,43 +8,38 @@
 ?>
 
 <div class="mediaPreviewComponent">
-    <div class="item-media-section">            
-        <?=$media->title?>
-    </div>
+  <div class="item-media-section">
+    <?=$media->title?>
+  </div>
 </div>
 <div class="row">
-    <div class="mediaPreviewComponent">
-        <div class="item-media-content">       
-            <!-- récupération de tous les médias -->
-            <?  foreach ($media->medias as $m):?>
-            <?
+  <div class="mediaPreviewComponent">
+    <!-- récupération de tous les médias -->
+    <?  foreach ($media->medias as $m):?>
+    <?
                 /* @var $m M_media */
             ?>
-            <div class="span2 item-media-data">
-                <div class="noGutter">
-                    <img src="<?=GiveMe::url($m->thumb)?>" alt="<?=$m->title?>" />
-                    <br/>
-                    <div class="item-media-name">
-                        <a href=""><?=$m->title?></a>
-                    </div>                    
-                    <a class="button" href="<?=$m->theFile->download()?>">
-                        <i class="icon-download"></i>
-                        Télécharger
-                    </a>
-                    <div class="clearfix"></div>
-                </div>
-            </div>         
-            <? endforeach;?>
-        </div>                
+    <div class="span2 item-media-data">
+      <div class="noGutter">
+      	<a href="" data-nav-is-ajax-target="mainContent" data-nav-is-ajax="true" class="thumbnail"><img src="<?=GiveMe::url($m->thumb)?>" alt="<?=$m->title?>" /></a>
+        <div class="item-media-content">
+          <div class="item-media-name"> <a href="" data-nav-is-ajax-target="mainContent" data-nav-is-ajax="true">
+            <?=$m->title?>
+            </a> </div>
+          <div class="item-media-link"> <a class="button" href="<?=$m->theFile->download()?>"> <i class="icon-download"></i> Télécharger</a>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-
-    <? /*
+    <? endforeach;?>
+  </div>
+  <? /*
 <div class="pagination">
           Pagination => <?=$subPagination?>
 </div>
     */?>
-
-<div class="pull-right">
-    View all
+  <div class="span8">
+    <div class="pull-right"> <a href="" data-nav-is-ajax-target="mainContent" data-nav-is-ajax="true" rel="nofollow">View all</a> </div>
+  </div>
 </div>
