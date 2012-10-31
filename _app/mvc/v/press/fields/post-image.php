14e1c69f85d7c023533aaf1dee9611b9fabc46db
Francois <francois@shic.fr>
2012-10-31 14:10:18 +0100

    Post content

--- _app/mvc/v/press/fields/post-image.php
+++ _app/mvc/v/press/fields/post-image.php
@@ -9,25 +9,26 @@ $field=$vv->field;
      data-field-type="File"
      data-field="root[thumb]"
      data-template="press/fields/post-image">
+    <div class="noGutter">
 
-    <?//thumb ?>
-    <img src="<?=$field->sized(180, 180, "000000", "jpg")?>" alt="<?/*=$vv->post->title*/?>" />
+        <?//thumb ?>
+        <img src="<?=$field->sized(180, 180, "000000", "jpg")?>" alt="<?/*=$vv->post->title*/?>" />
 
-    <?if($vv->isAdmin()):?>
-        <div class="wysiwyg">
-            <span class="btn-input-file">
-                            <input type="file" name="image_file" id="image_file" /><i class="icon-upload"></i>
-            </span>
+        <?if($vv->isAdmin()):?>
+            <div class="wysiwyg">
+                <span class="btn-input-file">
+                                <input type="file" name="image_file" id="image_file" /><i class="icon-upload"></i>
+                </span>
 
-             <input class="span2"
-               placeholder="select a file"
-               type="text"
-               value="<?=$vv->value?>">
+                 <input class="span2"
+                   placeholder="select a file"
+                   type="text"
+                   value="<?=$vv->value?>">
 
-            <div class="progress progress-striped">
-                <div class="bar" style="width: 0%;"></div>
+                <div class="progress progress-striped">
+                    <div class="bar" style="width: 0%;"></div>
+                </div>
             </div>
-        </div>
-    <?endif?>
-
+        <?endif?>
+    </div>
 </div>
--- _app/mvc/v/press/post.php
+++ _app/mvc/v/press/post.php
@@ -5,118 +5,127 @@
     /* @var $this View */
     $this->inside("press/layout", $vv);
 ?>
-<div class="post"
-     data-model-type="M_post"
-     data-model-id="<?=$vv->post->id?>"
-     data-model-refresh-controller="<?=C_press::post($vv->post->id,true)?>"
-     >
-    <br/>
-    <div class="row">
-        <div class="postPreviewComponent">
-            <div class="item-content">                                                              
-                <? // POST COMPONENT ?>
 
-                <div class="span8">
+<div class="row">
+    <div class="span8">
+        <div class="post noGutter background-content"
+             data-model-type="M_post"
+             data-model-id="<?=$vv->post->id?>"
+             data-model-refresh-controller="<?=C_press::post($vv->post->id,true)?>"
+             >
+            <br/>
+            <div class="row">
+                <div class="postPreviewComponent insidePost">
+                    <div class="item-content">                                                              
+                        <? // POST COMPONENT ?>
 
+                        <div class="span8">
+                            
+                            <div class="noGutter item-component">
 
+                                <div class="row">
 
-                    <div class="row">
+                                    <?if($vv->isAdmin()):?>
+                                        <div class="span8 mb1">
+                                        <a class="pull-right btn btn-success" href="#Model.save">Save</a>
+                                        </div>
+                                    <?endif?>
+
+                                    <?=$this->render("press/fields/post-image",$vv->thumbAdminField())?>
+
+                                    <div class="span6 item-text">
+                                        <div class="noGutter">
+                                            <?//title ?>
+                                            <div data-field="root[title]"
+                                                 data-field-type="Text">
+                                                <div class="item-title">
+                                                    <span
+                                                        <?//editable ?>
+                                                        <?=$vv->isAdmin()?"contenteditable='true' ":""?>
+                                                        <?// no text formatting allowed here */?>
+                                                            data-remove-format="true">
+                                                         <?=$vv->post->title?>
+                                                    </span>
+                                                </div>
+                                            </div>
+
+                                            <br/>
+                                            <?//description ?>
+                                            <div data-field="root[description]" data-field-type="Text">
+                                                <div class="item-description">
+                                                    <span
+                                                           <?//editable ?>
+                                                           <?=$vv->isAdmin()?"contenteditable='true' ":""?>
+                                                           <?// no text formatting allowed here */?>
+                                                           data-remove-format="true">
+                                                        <?=$vv->post->description?>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
 
                         <?if($vv->isAdmin()):?>
-                            <div class="span8 mb1">
-                            <a class="pull-right btn btn-success" href="#Model.save">Save</a>
-                            </div>
+                        <div class="offset2 span6 mt1">
+                            <a class=" btn btn-success btn-small" data-block-field-target="blocks" data-block-type="M_text" href="#Model.addBlock()">
+                                <i class="icon-white icon-plus-sign"></i>
+                                Add a text
+                            </a>
+
+                            <a class=" btn btn-success btn-small" data-block-field-target="blocks" data-block-type="M_embed" href="#Model.addBlock()">
+                                <i class="icon-white icon-plus-sign"></i>
+                                Add an embed
+                            </a>
+
+                            <a class=" btn btn-success btn-small" data-block-field-target="blocks" data-block-type="M_photo" href="#Model.addBlock()">
+                                <i class="icon-white icon-plus-sign"></i>
+                                Add a photo
+                            </a>
+                            <a class=" btn btn-success btn-small" data-block-field-target="blocks" data-block-type="M_media" href="#Model.addBlock()">
+                                <i class="icon-white icon-plus-sign"></i>
+                                Add a download
+                            </a>
+                        </div>
                         <?endif?>
 
-                        <?=$this->render("press/fields/post-image",$vv->thumbAdminField())?>
 
-                        <div class="span6 item-text">
-                            <?//title ?>
-                            <div data-field="root[title]"
-                                 data-field-type="Text">
-                                <div class="item-title">
-                                    <span
-                                        <?//editable ?>
-                                        <?=$vv->isAdmin()?"contenteditable='true' ":""?>
-                                        <?// no text formatting allowed here */?>
-                                            data-remove-format="true">
-                                         <?=$vv->post->title?>
-                                    </span>
-                                </div>
-                            </div>
+                        <!-- Start the block switcher -->
+                        <?  foreach ($vv->blocks as $bl):?>
+                            <? if ($bl->block->getContent()):?>
+                                <?=$this->render("press/blocks/" . $bl->block->modelType, $bl)?>
+                            <? endif;?>
+                        <?  endforeach; ?>
 
-                            <br/>
-                            <?//description ?>
-                            <div data-field="root[description]" data-field-type="Text">
-                                <div class="item-description">
-                                    <span
-                                           <?//editable ?>
-                                           <?=$vv->isAdmin()?"contenteditable='true' ":""?>
-                                           <?// no text formatting allowed here */?>
-                                           data-remove-format="true">
-                                        <?=$vv->post->description?>
-                                    </span>
+                        <div class="clearfix"></div>
+                        <br/>
+
+                        <? // SHARE COMPONENT ?>
+                        <div class="shareComponent">
+                            <div class="row">
+                                <?php $id = uniqid(); ?>
+                                <div class="span8">
+                                        <div class="item-share" id="<?= $id ?>">
+
+                                            <a class="item-email" data-page=""></a>
+                                            <?=GiveMe::socialGoogle()?>
+                                            <?=GiveMe::socialTwitter()?>
+                                            <?=GiveMe::socialFB()?>
+                                        </div>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="clearfix"></div>
+                        <br/>
                     </div>
                 </div>
-
-
-                <?if($vv->isAdmin()):?>
-                <div class="offset2 span6 mt1">
-                    <a class=" btn btn-success btn-small" data-block-field-target="blocks" data-block-type="M_text" href="#Model.addBlock()">
-                        <i class="icon-white icon-plus-sign"></i>
-                        Add a text
-                    </a>
-
-                    <a class=" btn btn-success btn-small" data-block-field-target="blocks" data-block-type="M_embed" href="#Model.addBlock()">
-                        <i class="icon-white icon-plus-sign"></i>
-                        Add an embed
-                    </a>
-
-                    <a class=" btn btn-success btn-small" data-block-field-target="blocks" data-block-type="M_photo" href="#Model.addBlock()">
-                        <i class="icon-white icon-plus-sign"></i>
-                        Add a photo
-                    </a>
-                    <a class=" btn btn-success btn-small" data-block-field-target="blocks" data-block-type="M_media" href="#Model.addBlock()">
-                        <i class="icon-white icon-plus-sign"></i>
-                        Add a download
-                    </a>
-                </div>
-                <?endif?>
-
-
-                <!-- Start the block switcher -->
-                <?  foreach ($vv->blocks as $bl):?>
-                    <? if ($bl->block->getContent()):?>
-                        <?=$this->render("press/blocks/" . $bl->block->modelType, $bl)?>
-                    <? endif;?>
-                <?  endforeach; ?>
-
-                <div class="clearfix"></div>
-                <br/>
-                
-                <? // SHARE COMPONENT ?>
-                <div class="shareComponent">
-                    <?php $id = uniqid(); ?>
-                    <div class="span8">
-                        <div class="item-share" id="<?= $id ?>">
-
-                            <a class="item-email" data-page=""></a>
-                            <?=GiveMe::socialGoogle()?>
-                            <?=GiveMe::socialTwitter()?>
-                            <?=GiveMe::socialFB()?>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="clearfix"></div>
-                <br/>
             </div>
+            <br/>
         </div>
     </div>
-    <br/>
 </div>
-
 
