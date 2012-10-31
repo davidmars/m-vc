2341457f4eaad45a1c0847bd7937da1494114b68
Francois <francois@shic.fr>
2012-10-31 01:15:57 +0100

    Pagination clean for category Post & Media

--- _app/mvc/v/press/paginationCategoryMedia.php
+++ _app/mvc/v/press/paginationCategoryMedia.php
@@ -6,32 +6,35 @@
 
 <br/>
 <br/>
-<!-- Insertion d'un composant de pagination -->
+
 <div class="paginationComponent">
     <div class="row">
         <div class="span8">
             <div class="noGutter">
                 <!-- Insertion d'un composant de titre -->
-                <div class="item-title"> More
-                    <?= $vv->categoryMedia->title ?>
-                </div>
+                <div class="item-title">More Downloads</div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
                 <!-- Insertion d'un composant de contenu -->
-                <div class="item-content">
-                    <div class="item-pagination">
-                        <div class="row">
-                            <div class="span8">
+        <div class="span8 item-content">
+            <div class="noGutter">
+                <div class="item-pagination">
+                    <div class="row">
+                        <? foreach($vv->pages as $page):?>
+                            <div class="span1 item-pagination-number <?= ($page->isCurrent) ? ("active") : ("") ?>">
                                 <div class="noGutter">
-                                    <? foreach($vv->pages as $page):?>
-                                    <div class="span1 item-pagination-number <?= ($page->isCurrent) ? ("active") : ("") ?>"> <a
-                                            href="<?=$page->href?>"
-                                            data-nav-is-ajax="true"
-                                            data-nav-is-ajax-target="mainContent">
+                                    <a
+                                        href="<?=$page->href?>"
+                                        data-nav-is-ajax="true"
+                                        data-nav-is-ajax-target="mainContent">
                                         <?= $page->name ?>
-                                    </a> </div>
-                                    <? endforeach; ?>
+
+                                    </a>
                                 </div>
                             </div>
-                        </div>
+                        <? endforeach; ?>
                     </div>
                 </div>
             </div>
